e78a408394cc3dc790cfcc8d335fdb473c8265d9
Micelovecat <flora_bux@mail.ru>
2021-12-18 04:21:55 +0300

    Fifth commit

--- src/ru/skypro/App1.java
+++ src/ru/skypro/App1.java
@@ -3,19 +3,19 @@ package ru.skypro;
 public class App1 {
     public static void main(String[] args) {
         String nameWarAndPeace = "Война и мир";
-        String authorWarAndPeace = "Л.Н. Толстой";
+        String authorWarAndPeace = "Л.Н. Толстой";    //лишний параметр, не входящий в условие задачи
         String firstNameAuthorWarAndPeace = "Лев";
         String lastNameAuthorWarAndPeace = "Толстой";
         int yearOfIssieWarAndPeace = 1996;
 
         String nameIdiot = "Идиот";
-        String authorIdiot = "Ф.М. Достоевский";
+        String authorIdiot = "Ф.М. Достоевский";      //лишний параметр, не входящий в условие задачи
         String firstNameAuthorIdiot = "Федор";
         String lastNameAuthorIdiot = "Достоевский";
         int yearOfIssieIdiot = 1990;
 
         String[] names = {"Война и мир", "Идиот"};
-        String[] authors = {"Л.Н. Толстой", "Ф.М. Достоевский"};
+        String[] authors = {"Л.Н. Толстой", "Ф.М. Достоевский"};   //лишний параметр, не входящий в условие задачи
         String[] firstNameAuthors = {"Лев", "Федор"};
         String[] lastNameAuthors = {"Толстой", "Достоевский"};
         int[] yearsOfIssie = {1996, 1990};
